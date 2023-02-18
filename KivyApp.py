5dbb9e2d3ab628d9cbebd51b68ca63bc08557ea5
Rashmars123 <66227003+Rashmars123@users.noreply.github.com>
2023-02-18 16:11:11 -0500

    Current FrontEnd of GUI
    Switched from Tkinter to Kivy for successful Android App Development

---
+++ KivyApp.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+import mysql.connector
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.widget import Widget
+
+# Designate Our .kv design file
+Builder.load_file('KivyApp.kv')
+
+
+class MainWidget(Widget):
+    checks = []
+    action = ' '
+
+    def checkbox_click(self, instance, value, topping):
+        if value:
+            MainWidget.checks.append(topping)
+            tops = ''
+            for x in MainWidget.checks:
+                tops = f'{tops} {x}'
+            MainWidget.action = x
+
+        else:
+            MainWidget.checks.remove(topping)
+            tops = ''
+            for x in MainWidget.checks:
+                tops = f'{tops} {x}'
+
+    def on_press_button(self):
+        if MainWidget.action == 'Create List':
+            pass
+        elif MainWidget.action == "Edit List":
+            pass
+        elif MainWidget.action == "Stock Prediction":
+            pass
+        else:
+            pass
+
+
+class MainApp(App):
+    def build(self):
+        return MainWidget()
+
+
+if __name__ == '__main__':
+    app = MainApp()
+    app.run()
