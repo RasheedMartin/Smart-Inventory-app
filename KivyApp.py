5950a27e603b2d34d4db4aff1c168f212600d9cd
Rashmars123 <66227003+Rashmars123@users.noreply.github.com>
2023-03-15 02:36:28 -0400

    Some updates

--- KivyApp.py
+++ KivyApp.py
@@ -1,4 +1,4 @@
-from barcode_reader import scan_barcodes
+from barcode_reader import scan_barcodes, get_price, create_database, update_database
 
 try:
     from kivy.app import App
@@ -9,6 +9,7 @@ except ImportError:
     from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 
 class MainWindow(Screen):
@@ -32,7 +33,7 @@ class MainWindow(Screen):
     def on_press_button(self):
 
         if MainWindow.action == 'Create List':
-            self.manager.current = "second"
+            self.manager.current = "creation"
             pass
         elif MainWindow.action == "Edit List":
             pass
@@ -42,26 +43,40 @@ class MainWindow(Screen):
             pass
 
 
-class SecondWindow(Screen):
+class CreationWindow(Screen):
 
     def on_release_button(self):
-        temp = scan_barcodes()
-        # if temp is None:
-        self.manager.current = 'third' # form
+        category = self.ids.newlist.text
+        # barcode_data = scan_barcodes()
+        barcode_data = "021200523588"
+        # Check the SQL data first
+        # If it exists, show it popup window that it already exists
+        # If it doesn't exist in SQL check the APIs
+        name, price = get_price(barcode_data)
+        # Go to the manual entry
+        if name or price is None:
+            # Pass the barcode and category to the third screen
+            self.pass_info(barcode_data, category)
+            self.manager.current = 'manualform'  # Go to manual Form
+        # It is successful and update the database
+        else:
+            # Display a successful popup window
+            update_database(name, barcode_data, price, category)
 
-        #database(temp)
-        pass
 
+class ManualWindow(Screen):
+    barcode_data = StringProperty("")
+    category = StringProperty("")
 
-class ThirdWindow(Screen):
-
-    def on_release_button1(self):
-        #database(temp)
-        pass
+    def on_release_button(self):
+        name = self.ids.newName.text
+        price = self.ids.newPrice.text
+        update_database(name, self.barcode_data, price, self.category)
 
 
 class WindowManager(ScreenManager):
-    pass
+    def pass_info(self, barcode_data, category):
+        self.add_widget(ManualWindow(barcode_data=barcode_data, category=category))
 
 
 # Designate Our .kv design file
@@ -74,5 +89,6 @@ class MainApp(App):
 
 
 if __name__ == '__main__':
+    create_database()
     app = MainApp()
     app.run()
--- barcode_reader.py
+++ barcode_reader.py
@@ -4,6 +4,7 @@ import pyzbar.pyzbar as pyzbar
 import requests
 from bs4 import BeautifulSoup
 import json
+import sqlite3
 
 
 # Define a function to capture video from your camera and process each frame
@@ -23,33 +24,102 @@ def scan_barcodes():
             barcode_data = obj.data.decode('utf-8')  # Save the decoded data to the variable
         # Display the processed frame
         cv2.imshow("Barcode Scanner", frame)
-        if barcode_data is not None:
-            # Website to use for searching barcode values
-            url = "https://api.barcodespider.com/v1/lookup"
-            querystring = {"upc": barcode_data}
-
-            headers = {
-                'token': "d970f9f75b5fb1d6c3a5",
-                'Host': "api.barcodespider.com",
-                'Accept-Encoding': "gzip, deflate",
-                'Connection': "keep-alive",
-                'cache-control': "no-cache"
-            }
-
-            response = requests.request("GET", url, headers=headers, params=querystring)
-            data = response.json()
-
-            prince = response.text
-
-            data_dict = json.loads(prince)
-            if data_dict['code'][0] == 404:
-                break
         # Press 'q' to quit
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cap.release()
-    # cv2.destroyAllWindows()
-    # Return the decoded barcode data`
+    cv2.destroyAllWindows()
+    return barcode_data  # Return the decoded barcode data`
 
 
+# Call the function to start scanning barcodes and save the result to a variable
+# barcode_result = scan_barcodes()
+# print(barcode_result)   # Print the decoded barcode data
+
+# Website to use for searching barcode values
+def get_price(barcode_result):
+    url = "https://api.barcodespider.com/v1/lookup"
+    querystring = {"upc": barcode_result}
+
+    headers = {
+        'token': "d970f9f75b5fb1d6c3a5",
+        'Host': "api.barcodespider.com",
+        'Accept-Encoding': "gzip, deflate",
+        'Connection': "keep-alive",
+        'cache-control': "no-cache"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    data = response.json()
+
+    price = response.text
+
+    data_dict = json.loads(price)
+    try: 
+        # Extract the price from the dictionary
+        price = data_dict['Stores'][0]['price']
+        name = data_dict['item_attributes']['title']
+
+        print(f"The name of item is {name}")
+    
+        return price, name
+    except KeyError: 
+        return None, None
+
+
+# print(get_price(barcode_result))
+
+# price, name = get_price(barcode_result)
+
+
+def create_database():
+    # Prompt the user for a database name
+    dbname = 'barcode_data'
+
+    # Create a connection to the database
+    conn = sqlite3.connect(dbname + ".db")
+
+    # Create a cursor object
+    cursor = conn.cursor()
+
+    # Create a table with three columns
+    cursor.execute('''CREATE TABLE products
+                      (UPC TEXT PRIMARY KEY,
+                       Name TEXT,
+                       Price REAL
+                       Category TEXT)''')
+
+    # Commit the table creation
+    conn.commit()
+
+    # Close the connection
+    conn.close()
+
+    print(f"Database {dbname}.db created with table 'products' containing columns 'UPC', 'Name', 'Price' and 'Category'.")
+
+
+# create_database()
+
+
+def update_database(name, barcode, price, category):
+    conn = sqlite3.connect("barcode_data.db")
+
+    cursor = conn.cursor()
+
+    # Insert data into the table
+    cursor.execute("INSERT INTO barcode_data (UPC, Name, Price, Category) VALUES (?, ?, ?, ?)", 
+                   (barcode, name, price, category))
+
+    # Commit the changes
+    conn.commit()
+
+    # Close the connection
+    conn.close()
+    return
+
+# update_database(sql_name,name, barcode_result, price)
+
+
+if __name__ == "__main__": 
+    pass
 
